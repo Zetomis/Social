5437623ef34f206081bdac6255df5632a3ca440f
Zetomis <duytan28042006@gmail.com>
2023-08-12 17:52:29 +0700

    init

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import "../globals.css";
+
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+};
+
+export default RootLayout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const RootPage = () => {
+    return <div>Rootpage</div>;
+};
+
+export default RootPage;
